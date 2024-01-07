6a57a349f79d5b3cadbb845c67176db5a8bffbca
Skryllzz <stevenmm09@gmail.com>
2024-01-06 19:42:02 -0500

    Inital Commit

---
+++ ss-server/Server.cmd
@@ -0,0 +1,4 @@
+@echo off
+title SS Server
+"C:\Program Files\java\jdk1.8.0_202\bin\java.exe" -jar Kronos.kronos-server.main.jar
+pause
---
+++ ss-server/run.cmd
@@ -0,0 +1,4 @@
+@echo off
+Title: SS
+"C:\Program Files\Java\jdk1.8.0_202\bin\java.exe" -jar Game-Server.jar
+pause
