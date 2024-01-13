29c24a5f6745746ea8f3c6952511294b6baab3f0
Skryllzz <stevenmm09@gmail.com>
2024-01-12 23:35:04 -0500

    Update: Added skip distance check for sawmill operator Effects: Varrock.java

--- ss-server/src/main/java/io/ruin/model/entity/npc/actions/Varrock.java
+++ ss-server/src/main/java/io/ruin/model/entity/npc/actions/Varrock.java
@@ -1,7 +1,11 @@
 package io.ruin.model.entity.npc.actions;
 
+import io.ruin.model.entity.shared.listeners.SpawnListener;
+
 public class Varrock {
 
     static {
+        SpawnListener.register(5422, npc -> npc.skipReachCheck = p -> p.equals(1264, 3500));
+        SpawnListener.register(5422, npc -> npc.skipReachCheck = p -> p.equals(3302, 3491));
     }
 }
