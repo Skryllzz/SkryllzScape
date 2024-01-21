0994ce02fefb84fb38549cc30bdaf6eb0db933a7
Skryllzz <stevenmm09@gmail.com>
2024-01-20 19:02:46 -0500

    Added Dark Mage and Koftik to West Ardy, Made some changes to Tindel Merchant Date: 1/20/2024 Files: ArdyNpcs.java

--- ss-server/src/main/java/io/ruin/model/entity/npc/actions/ArdyNpcs.java
+++ ss-server/src/main/java/io/ruin/model/entity/npc/actions/ArdyNpcs.java
@@ -1,37 +1,36 @@
 package io.ruin.model.entity.npc.actions;
 
-import io.ruin.cache.ItemID;
 import io.ruin.model.achievements.listeners.ardougne.ArdyEasy;
 import io.ruin.model.entity.npc.NPCAction;
-import io.ruin.model.inter.dialogue.ItemDialogue;
-import io.ruin.model.inter.dialogue.NPCDialogue;
-import io.ruin.model.inter.dialogue.OptionsDialogue;
-import io.ruin.model.inter.dialogue.PlayerDialogue;
+import io.ruin.model.inter.dialogue.*;
 import io.ruin.model.inter.utils.Option;
+import io.ruin.model.item.Item;
 import io.ruin.model.item.loot.LootItem;
 import io.ruin.model.item.loot.LootTable;
 import io.ruin.model.shop.ShopManager;
 
 import static io.ruin.cache.ItemID.*;
+import static io.ruin.model.item.actions.impl.chargable.Ibans.addChargesIban;
+import static io.ruin.model.item.actions.impl.chargable.Ibans.addChargesIbanU;
 
 public class ArdyNpcs {
 
     private static final LootTable TindelSwords = new LootTable()
             .addTable(2, //low level swords
-                    new LootItem(ItemID.IRON_LONGSWORD, 1, 1, 3),
-                    new LootItem(ItemID.IRON_SWORD, 1, 1, 3),
+                    new LootItem(IRON_LONGSWORD, 1, 1, 3),
+                    new LootItem(IRON_SWORD, 1, 1, 3),
                     new LootItem(BRONZE_SWORD, 1, 1, 3),
                     new LootItem(BRONZE_LONGSWORD, 1, 1, 3),
                     new LootItem(STEEL_SWORD, 1, 1, 1),
                     new LootItem(STEEL_LONGSWORD, 1, 1, 1)
             )
             .addTable(1, //medium level swords
-                    new LootItem(ItemID.BLACK_SWORD, 1, 1, 3),
-                    new LootItem(ItemID.BLACK_LONGSWORD, 1, 1, 1)
+                    new LootItem(BLACK_SWORD, 1, 1, 3),
+                    new LootItem(BLACK_LONGSWORD, 1, 1, 1)
             )
             .addTable(1, //high swords
-                    new LootItem(ItemID.MITHRIL_SWORD, 1, 1, 5),
-                    new LootItem(ItemID.MITHRIL_LONGSWORD, 1, 1, 5),
+                    new LootItem(MITHRIL_SWORD, 1, 1, 5),
+                    new LootItem(MITHRIL_LONGSWORD, 1, 1, 5),
                     new LootItem(COINS_995, 2000, 2000, 1)
             );
 
@@ -39,7 +38,7 @@ public class ArdyNpcs {
     static {
         /** Silk Merchant **/
         NPCAction.register(3211, "talk-to", (player, npc) -> {
-            if (player.getInventory().hasItem(ItemID.SILK, 1)) {
+            if (player.getInventory().hasItem(SILK, 1)) {
                 player.dialogue(
                         new PlayerDialogue("Hello. I have some fine silk from far away to sell to you."),
                         new NPCDialogue(npc, "Ah I may be interested in that. What sort of price were you looking at per piece of silk?"),
@@ -59,8 +58,8 @@ public class ArdyNpcs {
                                             new PlayerDialogue("I'll give it to you for 60."),
                                             new NPCDialogue(npc, "You drive a hard bargain, but I guess that will have to do."));
                                     int silkAmount = player.getInventory().getAmount(950);
-                                    player.getInventory().remove(ItemID.SILK, silkAmount);
-                                    player.getInventory().add(ItemID.COINS_995, silkAmount * 60);
+                                    player.getInventory().remove(SILK, silkAmount);
+                                    player.getInventory().add(COINS_995, silkAmount * 60);
                                     if (!ArdyEasy.isTaskCompleted(player, 3)) {
                                         ArdyEasy.completeTask(player, 3);
                                         player.sendMessage("<col=800000>Well done! You have completed an easy task in the Ardougne area. Your Achievement Diary has been updated.</col>");
@@ -93,6 +92,133 @@ public class ArdyNpcs {
                 }
             } else {
                 player.dialogue(new NPCDialogue(npc, "Sorry my friend, but you don't seem to have any swords that need to be identified."));
+            }
+        });
+
+        /** Koftik **/
+        NPCAction.register(3413, "talk-to", (player, npc) -> {
+            if (player.getCombat().getLevel() >= 60) {
+                player.dialogue(new PlayerDialogue("Hello Koftik, I understand you have the Iban's staff."),
+                                new NPCDialogue(npc, "Hello, yes I have do, however it broke during my last battle"),
+                                new PlayerDialogue("Oh no! Can it be fixed?."),
+                                new NPCDialogue(npc, "I know the Dark Mage east from here can but I couldn't afford his fee"),
+                                new NPCDialogue(npc,"If you want to give it a shot you can have it."),
+                                new OptionsDialogue(
+                                        new Option("Yes", () -> {
+                                            player.getInventory().add(1410, 1);
+                                            player.dialogue(new ItemDialogue().one(1410, "Koftik gives you the broken staff"),
+                                                    new PlayerDialogue("Thank you!"),
+                                                    new NPCDialogue(npc, "Good Luck!"));
+                                    }),
+                                        new Option("No", () -> {
+                                            player.dialogue(new PlayerDialogue("Not, right now"),
+                                                    new NPCDialogue(npc, "Let me know if you change your mind."));
+                                    })));
+        } else {
+                player.dialogue(new PlayerDialogue("Hello Koftik, I understand you have the Iban's Staff."),
+                                new NPCDialogue(npc, "Yes what's it to you?"),
+                                new MessageDialogue("Koftik won't help you until you have at least 60 combat level."));
+            }
+        });
+
+        /** Dark Mage **/
+        NPCAction.register(7752, "talk-to", (player, npc) -> {
+            if (player.getInventory().hasItem(IBANS_STAFF_1410, 1)) {
+                player.dialogue(new NPCDialogue(npc, "Can I help you with something, traveller? I am experimenting with dark magic; it is a dangerous craft, and I prefer not to be disturbed."),
+                        new PlayerDialogue("Can you repair my broken Staff of Iban?"),
+                        new ItemDialogue().one(1410, "You show the mage your broken staff."),
+                        new NPCDialogue(npc, "The Staff of Iban holds dangerous magic, traveller. I can fix it, but it will cost you - the process could kill me!"),
+                        new PlayerDialogue("How much?"),
+                        new NPCDialogue(npc, "200,000 coins. That'll give it 120 charges, enough to cast Iban's magic 120 times"),
+                        new OptionsDialogue(
+                                new Option("Repair the Staff", () -> {
+                                    if (player.getInventory().hasItem(995,200000)) {
+                                        player.getInventory().remove(995, 200000);
+                                        player.getInventory().remove(IBANS_STAFF_1410,1);
+                                        player.getInventory().add(IBANS_STAFF,1);
+                                        player.dialogue(new ItemDialogue().one(IBANS_STAFF, "The Mage repairs the staff and hands you it."));
+                                    } else {
+                                        player.dialogue(new PlayerDialogue("I seem to not have enough money on me"),
+                                                new NPCDialogue(npc, "You'll have to come back when you do."));
+                                    }
+                                }),
+                                new Option("No thanks", () -> {
+                                    player.dialogue(new PlayerDialogue("Not, right now"),
+                                            new NPCDialogue(npc, "Let me know if you change your mind."));
+                                })));
+            } else if (player.getInventory().hasItem(IBANS_STAFF, 1)) {
+                player.dialogue(new OptionsDialogue(
+                        new Option("Talk about Staff Upgrade", () -> {
+                            player.dialogue(new NPCDialogue(npc, "Can I help you with something, traveller? I am experimenting with dark magic; it is a dangerous craft, and I prefer not to be disturbed."),
+                                    new PlayerDialogue("Can you make it hold more than 120 charges?"),
+                                    new NPCDialogue(npc, "Hah! Yes, I can upgrade it to hold 2500 charges, enough to cast Iban's magic 2500 times. That'll cost you 400,000 coins."),
+                                    new OptionsDialogue(
+                                            new Option("Upgrade the Staff", () -> {
+                                                if (player.getInventory().hasItem(995, 400000)) {
+                                                    player.getInventory().remove(995, 400000);
+                                                    player.getInventory().remove(IBANS_STAFF, 1);
+                                                    player.getInventory().add(IBANS_STAFF_U, 1);
+                                                    player.dialogue(new ItemDialogue().one(IBANS_STAFF_U, "The Mage upgrades the staff and hands you it."));
+                                                } else {
+                                                    player.dialogue(new PlayerDialogue("I seem to not have enough money on me"),
+                                                            new NPCDialogue(npc, "You'll have to come back when you do."));
+                                                }
+                                            }),
+                                            new Option("No thanks", () -> {
+                                                player.dialogue(new PlayerDialogue("Not, right now"),
+                                                        new NPCDialogue(npc, "Let me know if you change your mind."));
+                                            })
+                                    )
+                            );
+                        }),
+                        new Option("Talk about recharging the staff", () -> {
+                            player.dialogue(new PlayerDialogue("Can you recharge my Staff of Iban?"),
+                                    new NPCDialogue(npc, "I'll want 100,000 coins to recharge the Staff of Iban."),
+                                    new OptionsDialogue(
+                                            new Option("Recharge the Staff", () -> {
+                                                if (player.getInventory().hasItem(995, 100000)) {
+                                                    player.getInventory().remove(995, 100000);
+                                                    Item ibans = player.getInventory().findItem(IBANS_STAFF); // Replace IBANS_STAFF with the actual item ID of the ibans staff
+                                                    if (ibans != null) {
+                                                        addChargesIban(player, ibans);
+                                                    }
+                                                } else {
+                                                    player.dialogue(new PlayerDialogue("I seem to not have enough money on me"),
+                                                            new NPCDialogue(npc, "You'll have to come back when you do."));
+                                                }
+                                            }),
+                                            new Option("No thanks", () -> {
+                                                player.dialogue(new PlayerDialogue("Not, right now"),
+                                                        new NPCDialogue(npc, "Let me know if you change your mind."));
+                                            })
+                                    )
+                            );
+                        })
+                ));
+            } else if (player.getInventory().hasItem(IBANS_STAFF_U,1)) {
+                player.dialogue(new PlayerDialogue("Can you recharge my Staff of Iban?"),
+                        new NPCDialogue(npc, "I'll want 100,000 coins to recharge the Staff of Iban."),
+                        new OptionsDialogue(
+                                new Option("Recharge the Staff", () -> {
+                                    if (player.getInventory().hasItem(995, 100000)) {
+                                        player.getInventory().remove(995, 100000);
+                                        Item ibans = player.getInventory().findItem(IBANS_STAFF_U); // Replace IBANS_STAFF with the actual item ID of the ibans staff
+                                        if (ibans != null) {
+                                            addChargesIbanU(player, ibans);
+                                        }
+                                    } else {
+                                        player.dialogue(new PlayerDialogue("I seem to not have enough money on me"),
+                                                new NPCDialogue(npc, "You'll have to come back when you do."));
+                                    }
+                                }),
+                                new Option("No thanks", () -> {
+                                    player.dialogue(new PlayerDialogue("Not, right now"),
+                                            new NPCDialogue(npc, "Let me know if you change your mind."));
+                                })
+                        )
+                );
+            } else {
+                player.dialogue(new NPCDialogue(npc, "Don't bother me I am experimenting with dark magic."));
             }
         });
 
