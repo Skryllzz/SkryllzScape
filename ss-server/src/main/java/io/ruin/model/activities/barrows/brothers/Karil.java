230acc5b40eb4fec20a47fd2ad082f6f0bdf56c6
Skryllzz <stevenmm09@gmail.com>
2024-01-22 20:46:42 -0500

    Fixing karils drain effect from Mage to Agility Date: 1/22/2024

--- ss-server/src/main/java/io/ruin/model/activities/barrows/brothers/Karil.java
+++ ss-server/src/main/java/io/ruin/model/activities/barrows/brothers/Karil.java
@@ -4,7 +4,6 @@ import io.ruin.api.utils.Random;
 import io.ruin.model.combat.AttackStyle;
 import io.ruin.model.entity.npc.NPCCombat;
 import io.ruin.model.map.Projectile;
-import io.ruin.model.stat.StatType;
 
 public class Karil extends NPCCombat {
 
@@ -24,7 +23,7 @@ public class Karil extends NPCCombat {
         projectileAttack(Projectile.BOLT, info.attack_animation, AttackStyle.RANGED, info.max_damage);
         if(Random.rollDie(4)) {
             target.graphics(401, 100, 0);
-            target.player.getStats().get(StatType.Magic).drain(5);
+            target.player.getMovement().drainEnergy(20);
         }
         return true;
     }
