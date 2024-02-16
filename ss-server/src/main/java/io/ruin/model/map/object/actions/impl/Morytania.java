295af4eba7a151d48c88e39b5e56d3bd7cf4b1c3
Skryllzz <stevenmm09@gmail.com>
2024-02-16 18:30:29 -0500

    Added Nature Spirit able to bless sickles. Date: 2/16/2024

--- ss-server/src/main/java/io/ruin/model/map/object/actions/impl/Morytania.java
+++ ss-server/src/main/java/io/ruin/model/map/object/actions/impl/Morytania.java
@@ -1,15 +1,22 @@
 package io.ruin.model.map.object.actions.impl;
 
 import io.ruin.model.World;
+import io.ruin.model.entity.npc.NPC;
+import io.ruin.model.entity.npc.NPCAction;
 import io.ruin.model.entity.player.Player;
+import io.ruin.model.inter.dialogue.ItemDialogue;
 import io.ruin.model.inter.dialogue.MessageDialogue;
+import io.ruin.model.inter.dialogue.NPCDialogue;
+import io.ruin.model.inter.dialogue.OptionsDialogue;
+import io.ruin.model.inter.utils.Option;
 import io.ruin.model.item.actions.ItemAction;
 import io.ruin.model.item.actions.ItemObjectAction;
 import io.ruin.model.map.Bounds;
 import io.ruin.model.map.object.GameObject;
 import io.ruin.model.map.object.actions.ObjectAction;
 
-import static io.ruin.cache.ItemID.SILVER_SICKLE_B;
+import static io.ruin.cache.ItemID.*;
+import static io.ruin.cache.NpcID.NATURE_SPIRIT;
 
 public class Morytania {
     private static final Bounds MORTLOGAREA = new Bounds(3422, 3331, 3444, 3358, 0);
@@ -32,6 +39,9 @@ public class Morytania {
         });
         ItemObjectAction.register(SILVER_SICKLE_B, "rotting log", (player, item, obj) -> updatelog(player, obj));
         ObjectAction.register(3509, "pick", Morytania::pick);
+        ObjectAction.register(3516, 3440, 3337, 0, "enter", (player, obj) -> player.getMovement().teleport(3442, 9734, 1));
+        ObjectAction.register(3526, 3442, 9733, 1, "exit", (player, obj) -> player.getMovement().teleport(3440, 3337, 0));
+        NPCAction.register(NATURE_SPIRIT, "talk-to", (player, npc) -> {natureSpirit(player);});
     }
 
     private static void replacelog(GameObject log) {
@@ -65,5 +75,36 @@ public class Morytania {
             player.sendMessage("You pick some mort myre fungi.");
             player.unlock();
         });
+    }
+
+    public static void bless(Player player) {
+        NPC npc = new NPC(NATURE_SPIRIT);
+        if (player.getInventory().hasItem(SILVER_SICKLE,1)) {
+        if (player.getInventory().hasItem(COINS_995, 50000)) {
+        player.startEvent(event -> {
+            player.lock();
+            player.dialogue(new NPCDialogue(NATURE_SPIRIT, "Certainly!."),
+                    new ItemDialogue().one(SILVER_SICKLE, "You hand your sickle to the spirit."));
+            event.delay(2);
+            player.getInventory().remove(SILVER_SICKLE,1 );
+            player.dialogue(new ItemDialogue().one(SILVER_SICKLE_B, "Spirit hands you the blessed sickle."));
+            event.delay(2);
+            player.getInventory().add(SILVER_SICKLE_B);
+            player.unlock();
+            });
+        } else {
+            player.dialogue(new NPCDialogue(NATURE_SPIRIT, "I'm afraid you don't have enough coins."));
+        }
+        } else {
+            player.dialogue(new MessageDialogue( "You don't have any sickles on you."));
+        }
+    }
+
+    public static void natureSpirit(Player player) {
+        player.dialogue(new NPCDialogue(NATURE_SPIRIT, "Hello " + player.getName() + ", how may I help you?"),
+                        new OptionsDialogue("Choose an Option:",
+                        new Option("Can you bless my sickle? (50k coins)", () -> bless(player)),
+                        new Option("Nothing at the moment.")));
+
     }
 }
