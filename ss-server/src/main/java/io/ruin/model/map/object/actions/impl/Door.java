37c57b6e358ddfc979038462b444ef99edb51a7f
Skryllzz <stevenmm09@gmail.com>
2024-02-19 18:53:14 -0500

    You can open the door now to grums shop Date: 2/19/2024

--- ss-server/src/main/java/io/ruin/model/map/object/actions/impl/Door.java
+++ ss-server/src/main/java/io/ruin/model/map/object/actions/impl/Door.java
@@ -556,7 +556,8 @@ public class Door {
                 {13346, 13352}, {13347, 13353}, // steel dungeon door
                 {13348, 13354}, {13349, 13355}, // marble dungeon door
                 {9038, 9039}, //Karamja teak tree entrance
-                {1511, 1511}, {1513, 1513}
+                {1511, 1511}, {1513, 1513},
+                {58, 59}
         };
         /**
          * These objects face 180 degrees different than others.
